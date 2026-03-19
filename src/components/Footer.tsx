50b0bee480b33de0e949250ae94f42a95861f587
Your Name <you@example.com>
2026-03-19 12:04:50 -0600

    chore: update footer logo to horizontal version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import brandLogo from '../assets/logo_footer.png';
+import brandLogo from '../assets/logo_footer_horizontal.png';
 import { Instagram, Linkedin, Music2 } from 'lucide-react';
 
 export const Footer: React.FC = () => {
